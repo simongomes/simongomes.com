be99be7a2ff9f1ff10132854d4d1e2595dcc7e53
Simon Gomes <busy.s.simon@gmail.com>
2019-08-28 17:50:00 +0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <p>I am Simon</p>
+        </div>
+    );
+};
+
+export default Home;
