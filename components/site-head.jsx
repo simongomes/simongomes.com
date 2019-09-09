f70cbe1133c7053d606c649273a05f80c760a47b
Simon Gomes <busy.s.simon@gmail.com>
2019-09-09 23:21:42 +0600

    google analytics added

--- components/site-head.jsx
+++ components/site-head.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Head from 'next/head';
+import { initGA, logPageView } from '../src/js/googleAnalytics';
 
-const SiteHead = () => {
-  return (
-    <Head>
-      <title>Simon Gomes - Full-Stack Developer</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta
-        name="description"
-        content="Experienced Full-Stack Web Application Developer, specialized in JavaScript, PHP, moderate
+class SiteHead extends Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    return (
+      <Head>
+        <title>Simon Gomes - Full-Stack Developer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Experienced Full-Stack Web Application Developer, specialized in JavaScript, PHP, moderate
         understand on Node for server-side scripting and API development as well as Database Management and
         architecture."
-      />
-      <meta name="og:title" content="Simon Gomes" />
-      <meta
-        name="og:description"
-        content="Experienced Full-Stack Web Application Developer, specialized in JavaScript, PHP, moderate
+        />
+        <meta name="og:title" content="Simon Gomes" />
+        <meta
+          name="og:description"
+          content="Experienced Full-Stack Web Application Developer, specialized in JavaScript, PHP, moderate
       understand on Node for server-side scripting and API development as well as Database Management and
       architecture."
-      />
+        />
 
-      <link rel="shortcut icon" href="static/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="static/favicon.ico" type="image/x-icon" />
-      <meta name="theme-color" content="#ffd201" />
-    </Head>
-  );
-};
+        <link
+          rel="shortcut icon"
+          href="static/favicon.ico"
+          type="image/x-icon"
+        />
+        <link rel="icon" href="static/favicon.ico" type="image/x-icon" />
+        <meta name="theme-color" content="#ffd201" />
+      </Head>
+    );
+  }
+}
 
 export default SiteHead;
